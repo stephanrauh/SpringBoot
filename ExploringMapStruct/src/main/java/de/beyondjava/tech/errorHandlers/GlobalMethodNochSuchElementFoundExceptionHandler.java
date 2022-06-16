c7aee5487d68a4aa17b90efb70336cd80e54a643
Stephan Rauh <3045767+stephanrauh@users.noreply.github.com>
2022-06-16 23:07:25 +0200

    fixed a weird merge conflicht

--- ExploringMapStruct/src/main/java/de/beyondjava/tech/errorHandlers/GlobalMethodNochSuchElementFoundExceptionHandler.java
+++ ExploringMapStruct/src/main/java/de/beyondjava/tech/errorHandlers/GlobalMethodNochSuchElementFoundExceptionHandler.java
@@ -1,54 +1,21 @@
 package de.beyondjava.tech.errorHandlers;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.ToString;
+
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
-import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.NoSuchElementException;
 
-import static org.springframework.http.HttpStatus.BAD_REQUEST;
+import static org.springframework.http.HttpStatus.NOT_FOUND;
 
-/**
- * Kudos https://stackoverflow.com/questions/33663801/how-do-i-customize-default-error-message-from-spring-valid-validation
- * Kudos http://www.petrikainulainen.net/programming/spring-framework/spring-from-the-trenches-adding-validation-to-a-rest-api/
- */
 @Order(Ordered.HIGHEST_PRECEDENCE)
 @ControllerAdvice
-public class GlobalMethodArgumentNotValidExceptionHandler {
-
-    @ResponseStatus(BAD_REQUEST)
-    @ResponseBody
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ValidationErrorList methodArgumentNotValidException(MethodArgumentNotValidException ex) {
-        var result = ex.getBindingResult();
-        var fieldErrors = result.getFieldErrors();
-        return processFieldErrors(fieldErrors);
-    }
-
-    private ValidationErrorList processFieldErrors(List<org.springframework.validation.FieldError> fieldErrors) {
-        var error = new ValidationErrorList(BAD_REQUEST.value(), fieldErrors.size() + " validation error(s) found.");
-        for (var fieldError : fieldErrors) {
-            error.getFieldErrors().add(new ValidationError(fieldError.getObjectName(), fieldError.getField(),
-                    fieldError.getDefaultMessage()));
-        }
-        return error;
-    }
-
-
-    @Getter
-    @AllArgsConstructor
-    @ToString
-    public static class ValidationErrorList {
-        private final int status;
-        private final String message;
-        private final List<ValidationError> fieldErrors = new ArrayList<>();
+public class GlobalMethodNochSuchElementFoundExceptionHandler {
+    @ExceptionHandler(NoSuchElementException.class)
+    public ResponseEntity<String> noSuchElementException(NoSuchElementException ex) {
+        return new ResponseEntity("Couldn't find the resource", NOT_FOUND);
     }
 }
